9f3b9dc95a91c9e116db2f6f15b5a517067925a2
Doug Wiegley <dougw@a10networks.com>
2014-08-13 20:35:16 -0600

    script fixes

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 
-import os
-
 devices = {
     "ax-lsi": {
         "host": "10.0.0.160",
         "username": "admin",
-        "password": os.environ['AX_PASSWORD'],
+        "password": "ooQk3VoDVz8GabDQZeugMQYq",
         "port": 8443,
         "api_version": "2.1",
     },
